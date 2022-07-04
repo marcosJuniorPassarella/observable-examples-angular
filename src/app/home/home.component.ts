8867842217f8a64667f7278ac595da1f71a9ffc5
MJWORKS <mjworks2020@outlook.com>
2022-07-04 20:18:28 -0300

    Implemented the manualy observable for a example

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  private firstObsSubscription!: Subscription;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    interval(1000).subscribe({
+      next: (count) => {
+        console.log(count);
+      },
+    });
   }
 
+  ngOnDestroy(): void {
+    this.firstObsSubscription.unsubscribe();
+  }
 }
