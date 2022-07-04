98017046abaa3fe2a232580e6d00913a3a59f95e
MJWORKS <mjworks2020@outlook.com>
2022-07-04 15:35:21 -0300

    Added new changes

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
+  styleUrls: ['./user.component.scss'],
 })
 export class UserComponent implements OnInit {
+  id!: number;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params['id'];
+    });
   }
-
 }
